a2fe47e115456aefad11f2dcc90f717e08518b94
Jonathan Beatty <easyegoism@gmail.com>
2016-11-01 06:31:54 -0500

    bring IndexController.php up to standards

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -6,13 +6,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * Returns the index page at /
+ */
 class IndexController extends Controller
 {
-	/**
-	 * @Route("/")
-	 */
-	public function indexAction()
-	{
-		return $this->render('index.html.twig');
-	}
+    /**
+     * @Route("/")
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('index.html.twig');
+    }
 }
