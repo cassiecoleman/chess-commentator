9ac778ba290c159ba77801106f12a8b00770d75d
Jonathan Beatty <easyegoism@gmail.com>
2016-11-01 04:56:58 -0500

    add index page inheriting from base twig template

--- src/AppBundle/Controller/IndexController.php
+++ src/AppBundle/Controller/IndexController.php
@@ -6,15 +6,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class IndexController extends Controller;
+class IndexController extends Controller
 {
 	/**
 	 * @Route("/")
 	 */
 	public function indexAction()
 	{
-		return new Response(
-			'<html><body>Hello world!</html></body>'
-		);
+		return $this->render('index.html.twig');
 	}
 }
