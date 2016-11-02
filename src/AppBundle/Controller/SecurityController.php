f77c8c32098460f33d95118874d6b3b521ed29c1
Jonathan Beatty <easyegoism@gmail.com>
2016-11-02 05:04:35 -0500

    finish login system and form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/**
+ * Controller for login form
+ */
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @param Request $request
+     * @return render login page
+     */
+    public function loginAction(Request $request)
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // Last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
+    }
+}
