7c1d3d46e21528ff34ef90427e3f36cf77844c0e
Jonathan Beatty <easyegoism@gmail.com>
2016-11-02 03:48:09 -0500

    add fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+
+class LoadUserData implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $userAdmin = new User();
+        $userAdmin->setUsername('admin');
+        $userAdmin->setPassword('test');
+        $userAdmin->setEmail('test@email.com');
+
+        $manager->persist($userAdmin);
+        $manager->flush();
+    }
+}
